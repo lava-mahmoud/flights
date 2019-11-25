dd599c7585017469629e3d701ff9527f3d8e5330
root <lava.mahmoud89@gmail.com>
2019-11-25 11:09:16 +0100

    Updates

---
+++ precache-manifest.fbeeb4c2d911c9801e62b74493e48990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f05f2d1742b84e879e3bf1a5778390",
+    "url": "/flights/index.html"
+  },
+  {
+    "revision": "cf0439b86b7a82ad74fb",
+    "url": "/flights/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2c0d9be49349c3bbf0ba",
+    "url": "/flights/static/js/2.0c4b2099.chunk.js"
+  },
+  {
+    "revision": "cf0439b86b7a82ad74fb",
+    "url": "/flights/static/js/main.8a427b27.chunk.js"
+  },
+  {
+    "revision": "7729ed66e13b75f11ed5",
+    "url": "/flights/static/js/runtime-main.7785733e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/flights/static/media/logo.25bf045c.svg"
+  }
+]);
